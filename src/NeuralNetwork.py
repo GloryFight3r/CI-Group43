b1f9b486f50a23d44d89ca197fa7f798b47fcb46
Borislav Semerdzhiev <borislavsemerdzhiev.02@gmail.com>
2023-03-05 20:40:21 +0100

    Finishes the back propagation Solves the XOR problem

--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -10,14 +10,18 @@ class Perceptron:
     b : float
     z : float
     gradient : float
+    b_gradient : float
     def __init__(self, initial_weight: np.ndarray, initial_bias: float, activation_function: Functions.ActivationFunction):
         self.w = initial_weight
+        #print("Initial", self.w)
         self.b = initial_bias
         self.gradient = 0
+        self.b_gradient = 0
         self.activation_function = activation_function
     
     def forward(self, input : np.ndarray):
         self.x = input
+        #print(self.w, input)
         self.z = self.w.dot(input) + self.b
         self.a = self.activation_function.f(self.z)
         return self.a
@@ -34,6 +38,7 @@ class Layer:
     input_size : int
     layer_dim : int
     derivatives : np.ndarray
+    b_derivatives : np.ndarray
     perceptrons : list[Perceptron]
 
     def __init__(self, input_size: int, layer_dim: int):
@@ -41,68 +46,110 @@ class Layer:
         self.layer_dim = layer_dim
 
         self.derivatives = np.zeros(layer_dim)
-        self.perceptrons = [Perceptron(np.zeros(input_size), 0, Sigmoid) for i in range(layer_dim)]
+        self.b_derivatives = np.zeros(layer_dim
+                                      )
+        self.perceptrons = [Perceptron(np.random.rand(input_size), 0.1, Functions.Sigmoid) for i in range(layer_dim)]
     def calculate(self, input : np.ndarray):
         output = np.zeros(self.layer_dim)
 
         for ind, perceptron in enumerate(self.perceptrons):
             output[ind] = perceptron.forward(input)
         return output
+    def flush_derivative(self, count:int, learning_rate:float):
+        for perceptron in self.perceptrons:
+            #print(perceptron.w, perceptron.b)
+
+            perceptron.w = perceptron.w + (learning_rate * perceptron.gradient / count)
+            perceptron.b = perceptron.b + (learning_rate * perceptron.b_gradient / count)
+
+            perceptron.gradient = 0
+            perceptron.b_gradient = 0
+
+            #print(perceptron.w, perceptron.b)
 
 class ANN:
     layers : list[Layer]
     layers_count : int
-    def __init__(self, dimensions):
+    def __init__(self, input_dimension : int, dimensions : list[int]):
         self.layers = []
         self.layers_count = len(dimensions)
-        for ind, sz in enumerate(dimensions):
-            self.layers.append(Layer(dimensions[ind - 1], sz))
+        
+        self.layers.append(Layer(input_dimension, dimensions[0]))
+    
+        for ind, sz in enumerate(dimensions[1:]):
+            self.layers.append(Layer(dimensions[ind], sz))
             
 
     def calc_derivatives(self, expected_output : np.ndarray):
         # precalculate derivates of last layer
-        last_layer_size = len(self.layers[self.layers_count - 1])
+        last_layer_size = len(self.layers[self.layers_count - 1].perceptrons)
+        
         for i in range(last_layer_size):
             self.layers[self.layers_count - 1].derivatives[i] = 2 * (expected_output[i] - self.layers[self.layers_count - 1].perceptrons[i].a)
+            self.layers[self.layers_count - 1].b_derivatives[i] = 2 * (expected_output[i] - self.layers[self.layers_count - 1].perceptrons[i].a)
 
         # we start from layer_size - 1, ignoring the output layer
-        for l in range(len(self.layers) - 2, 0, -1):
+        for l in range(len(self.layers) - 2, -1, -1):
             for j in range(len(self.layers[l].perceptrons)):
+                self.layers[l].b_derivatives[j] = 0
+                self.layers[l].derivatives[j] = 0
                 for i in range(len(self.layers[l + 1].perceptrons)):
                     z_i = self.layers[l + 1].perceptrons[i].z
                     
-                    current_addition = 0
                     current_addition = self.layers[l + 1].perceptrons[i].w[j]
                     current_addition *= self.layers[l + 1].perceptrons[i].activation_function.d(z_i) 
                     current_addition *= self.layers[l + 1].derivatives[i]
-                    
+
                     self.layers[l].derivatives[j] += current_addition
+
+                    #current_addition = self.layers[l + 1].perceptrons[i].b
+                    current_addition *= self.layers[l + 1].perceptrons[i].activation_function.d(z_i) 
+                    current_addition *= self.layers[l + 1].b_derivatives[i]
+
+                    self.layers[l].b_derivatives[j] += current_addition
+
     def predict(self, input : np.ndarray):
         for cur_layer in self.layers:
             input = cur_layer.calculate(input)
         return input
 
-    def back_propagate(self, input : np.ndarray, expected_output : np.ndarray, learning_rate : float):
+    def back_propagate(self, input : np.ndarray, expected_output : np.ndarray):
         self.calc_derivatives(expected_output)
         
         # backpropagate all layers except the first one - as it requires the input
-        for l in range(len(self.layers) - 1, 1, -1):
+        for l in range(len(self.layers) - 1, 0, -1):
+            #print(l)
             for j in range(len(self.layers[l].perceptrons)):
                 for k in range(len(self.layers[l - 1].perceptrons)):
-                    to_calc = self.layers[l - 1].perceptrons[k].a
-                    to_calc *= self.layers[l].perceptrons[j].z
+                    to_calc = self.layers[l].perceptrons[j].activation_function.d(self.layers[l].perceptrons[j].z)
                     to_calc *= self.layers[l].derivatives[j]
+
+                    to_calc *= self.layers[l - 1].perceptrons[k].a
                     
-                    self.layers[l].perceptrons[j].gradient += to_calc * learning_rate
+                    self.layers[l].perceptrons[j].gradient += to_calc
+
+                    to_calc = self.layers[l].perceptrons[j].activation_function.d(self.layers[l].perceptrons[j].z)
+                    to_calc *= self.layers[l].b_derivatives[j]
+
+                    self.layers[l].perceptrons[j].b_gradient += to_calc
         
         # back propagate layer 0
-        for perceptron in self.layers[0].perceptrons:
+        for ind2, perceptron in enumerate(self.layers[0].perceptrons):
             for ind in range(len(perceptron.w)):
-                to_calc = input[ind]
-                to_calc *= self.layers[0].perceptrons[ind].z
-                to_calc *= self.layers[0].derivatives[ind]
-                    
-                perceptron.gradient += to_calc * learning_rate
+                to_calc = perceptron.activation_function.d(perceptron.z)
+                to_calc *= self.layers[0].derivatives[ind2]
+                to_calc *= input[ind]
+
+                perceptron.gradient += to_calc
+
+                to_calc = perceptron.activation_function.d(perceptron.z)
+                to_calc *= self.layers[0].derivatives[ind2]
+
+                perceptron.b_gradient += to_calc
+
+    def flush_derivatives(self, count:int, learning_rate:float):
+        for layer in self.layers:
+            layer.flush_derivative(count, learning_rate)
             
 '''
 class GCN(torch.nn.Module):
