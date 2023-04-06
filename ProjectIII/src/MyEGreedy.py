59f542aee4499bd7a48858fe9f8178f1b553b978
Yehor <yehor.kozyr@gmail.com>
2023-04-06 13:43:23 +0200

    Added random choice when there are multiple best values in best_action

---
+++ ProjectIII/src/MyEGreedy.py
@@ -0,0 +1,40 @@
+from Agent import Agent
+from Action import Action
+from Maze import Maze
+from QLearning import QLearning
+import numpy as np
+
+class MyEGreedy:
+    def __init__(self):
+        print("Made EGreedy")
+
+    def get_random_action(self, agent:Agent, maze:Maze):
+        # TODO to select an action at random in State s
+        actions = maze.get_valid_actions(agent)
+        
+        rnd = np.random.randint(len(actions))
+
+        return actions[rnd]
+
+    def get_best_action(self, agent:Agent, maze:Maze, q_learning:QLearning):
+        # TODO to select the best possible action currently known in State s.
+        actions = maze.get_valid_actions(agent)
+
+        all_rewards = []
+            
+        for x in actions:
+            newAgent = agent
+            newAgent.do_action(x, maze)
+            cur_reward = q_learning.get_q(newAgent.get_state(maze), x)
+            all_rewards.append(cur_reward)
+
+        return np.random.choice(np.flatnonzero(all_rewards == all_rewards.max()))
+        
+
+    def get_egreedy_action(self, agent, maze, q_learning, epsilon):
+        # TODO to select between random or best action selection based on epsilon.
+        rnd = np.random.rand()
+        if rnd < epsilon:
+            return self.get_random_action(agent, maze)
+        else:
+            return self.get_best_action(agent, maze, q_learning)
